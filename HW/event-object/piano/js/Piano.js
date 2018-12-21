4b6ed2ab1ff8d1059fb17ea915986a8780c594db
vertjs <45886652+vertjs@users.noreply.github.com>
2018-12-21 16:58:21 +0300

    21/12/18

--- HW/event-object/piano/js/Piano.js
+++ HW/event-object/piano/js/Piano.js
@@ -5,20 +5,44 @@ const higher = [
 'sounds/higher/first.mp3',
 'sounds/higher/fourth.mp3',
 'sounds/higher/second.mp3',
-'sounds/higher/fhird.mp3',
+'sounds/higher/third.mp3',
 ];
 
-const btns = document.getElementsByTagName('li')
 
+const middle = [
+'sounds/middle/fifth.mp3',
+'sounds/middle/first.mp3',
+'sounds/middle/fourth.mp3',
+'sounds/middle/second.mp3',
+'sounds/middle/third.mp3'
+]
+
+
+
+const btns = document.getElementsByTagName('li');
 for(var key of btns) {
-	key.addEventListener('click', play)
+	key.addEventListener('click', play);
 }
 
 
 function play(event) {
-	const audio = event.currentTarget.getElementsByTagName('audio')[0];
-	audio.src = higher[0];
+	document.addEventListener('keydown', up);
+	function up(event) {		
+		switch (event.key) {
+			case 'Shift':
+				for(var i = 0; i <  btns.length; i++) {
+					btns[i].getElementsByTagName('audio')[0].src = higher[i];
+				}
+			break;
+
+			default:
+			for(var i = 0; i <  btns.length; i++) {
+				btns[i].getElementsByTagName('audio')[0].src = middle[i];
+			}
+		}
+	}
+	var audio = this.getElementsByTagName('audio')[0];
+	audio.currentTime = 0;
 	audio.play();
-		
-	console.log(audio)
+	console.log(audio);
 }
