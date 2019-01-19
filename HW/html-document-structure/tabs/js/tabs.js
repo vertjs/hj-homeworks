a0d227ea3b7d4c84c885d91ce1198cc5e7f25462
vertjs <lena.vertieva@gmail.com>
2019-01-19 14:35:01 +0300

    19/01

--- HW/html-document-structure/tabs/js/tabs.js
+++ HW/html-document-structure/tabs/js/tabs.js
@@ -1,36 +1,45 @@
 'use strict';
 
 const tabs = document.querySelector('.tabs-nav');
-const tab1 = tabs.firstElementChild;
-const tab2 = tab1.cloneNode(true);
-const tab3 = tab1.cloneNode(true);
-tabs.appendChild(tab2);
-tabs.appendChild(tab3);
-const listTabs = tabs.querySelectorAll('li');
-const listA = tabs.querySelectorAll('a');
-const arrListTabs = Array.from(listTabs); // массив вкладок
-const arrListA = Array.from(listA); // массив тегов 'a'
 const tabs_content = document.querySelectorAll('[data-tab-title]');
 const arrContentTabs = Array.from(tabs_content); //массив текстов
-document.addEventListener('DOMContentLoaded', primaryDisplay);
 
-function primaryDisplay(event) {
-  arrContentTabs.forEach(el => el.classList.add('hidden'));
-  arrContentTabs[0].classList.remove('hidden');
-  arrListTabs[0].classList.add('ui-tabs-active');
-}
+arrContentTabs.forEach(function(el, i) {
+  tabs.appendChild(tabs.firstElementChild.cloneNode(true));
+})
 
-for(var i = 0; i < tabs_content.length; i ++) {
+tabs.removeChild(tabs.firstElementChild);
+
+const listTabs = tabs.querySelectorAll('li');
+const listA = tabs.querySelectorAll('a'); //4
+const arrListA = Array.from(listA); //4
+const arrListTabs = Array.from(listTabs); // массив вкладок
+
+
+for(var i = 0; i < tabs_content.length; i++) {
   arrListA[i].textContent = tabs_content[i].getAttribute("data-tab-title");
   arrListA[i].classList.add(tabs_content[i].getAttribute("data-tab-icon"));
 }
-arrListTabs.forEach(el => el.addEventListener('click', showContent))
 
-var ind=0;
+
+arrListTabs.forEach(el => el.addEventListener('click', showContent))
+showContent.call(tabs.firstElementChild)
+
+
 function showContent(event) {
-  arrListTabs.forEach(el => el.classList.remove('ui-tabs-active'));
-  arrContentTabs.forEach(el => el.classList.add('hidden'));
-  event.currentTarget.classList.add('ui-tabs-active');
-  ind = arrListTabs.indexOf(event.currentTarget)
-  tabs_content[ind].classList.remove('hidden');
+  const tab = event ? event.currentTarget : this;
+
+  if(tabs.querySelector('.ui-tabs-active')) {
+    tabs.querySelector('.ui-tabs-active').classList.remove('ui-tabs-active')
+  } else {
+    tab.classList.add('ui-tabs-active');
+  }
+
+  arrContentTabs.forEach(el => {
+    if (el.dataset.tabTitle === tab.firstElementChild.textContent) {
+            el.classList.remove('hidden');
+        } else {
+            el.classList.add('hidden');
+        }
+  })
 }
