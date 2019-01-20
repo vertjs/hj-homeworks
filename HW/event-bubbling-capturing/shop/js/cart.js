9cf99d672c6b947057588f39373570ebad9c4272
vertjs <lena.vertieva@gmail.com>
2019-01-20 15:46:14 +0300

    20/01

--- HW/event-bubbling-capturing/shop/js/cart.js
+++ HW/event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', loaded);
+
+function loaded(event) {
+  const but = document.querySelectorAll('.add-to-cart');
+  Array.from(but).forEach(el => {
+    el.addEventListener('click', event => addToCart(el));
+    el.title = el.getAttribute("data-title");
+    el.price = el.getAttribute("data-price");
+  })
+};
+
+showMore.addEventListener('click', event => {
+  event.preventDefault();
+  loaded(event)
+});
--- HW/event-bubbling-capturing/table/js/sort.js
+++ HW/event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,24 @@
 'use strict';
 
+var n = 1;
+
 function handleTableClick(event) {
-  
+  if(event.target.tagName === "TH") {
+    if(n === 1) {
+        var attribute = event.target.getAttribute("data-prop-name");
+        n=-1;
+        sortTable(attribute, n);
+        event.currentTarget.setAttribute('data-sort-by', attribute);
+        event.target.setAttribute('data-dir', n);
+        return n;
+      } else if(n === -1) {
+        n=1;
+        var attribute = event.target.getAttribute("data-prop-name");
+        sortTable(attribute, n);
+        event.currentTarget.setAttribute('data-sort-by', attribute);
+        event.target.setAttribute('data-dir', n);
+
+        return n;
+      }
+  } else return;
 }
