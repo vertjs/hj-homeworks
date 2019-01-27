fa2098fa360c90ce6fd49c9c655d43ebc9870c3a
vertjs <lena.vertieva@gmail.com>
2019-01-27 18:13:50 +0300

    27/12

--- HW/local-storage/cart/js/cart.js
+++ HW/local-storage/cart/js/cart.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const xhr = new XMLHttpRequest();
+let access, block;
+const thumbs = document.getElementsByClassName('thumbs')[0];
+let bigImage = document.getElementById('big-image');
+let price = 0;
+let counte = 0;
+let item = document.getElementById('quick-cart');
+Array.from(thumbs.children).forEach(el => el.addEventListener('click', doActiveImg));
+
+function doActiveImg(event) { // выделение фоток
+  event.preventDefault();
+  Array.from(thumbs.children).forEach(el => el.classList.remove('active'))
+  event.currentTarget.classList.add('active');
+  bigImage.style.backgroundImage = `url("${event.currentTarget.href}")`;
+  console.log(bigImage);
+};
+
+const xhr = new XMLHttpRequest(); // добавление цветов
 xhr.open('GET', 'https://neto-api.herokuapp.com/cart/colors');
 xhr.send();
 xhr.addEventListener('load', onLoad);
@@ -9,55 +25,38 @@ function onLoad() {
   var result = JSON.parse(xhr.response)
   var item = document.querySelector('#colorSwatch')
   for(var i=0; i < result.length; i++) {
-    addForm(result[i])
+    addFormColors(result[i])
+    const colorElements = item.querySelectorAll('input');
+    for (const elem of colorElements) {
+      elem.checked = elem.value === localStorage.getItem('color');
+    };
   }
-  function addForm(data) {
+  function addFormColors(data) {
+    lockVariable(data);
     var item2 = document.createElement('div')
     item2.dataset.value = data.type;
     item2.className ='swatch-element color';
     item2.classList.add(data.type)
-    if(data.isAvailable){
-      item2.classList.add('available')
-    } else{
-      item2.classList.add('soldout')
+    item2.classList.add(access)
+    item.appendChild(item2);
+    let colorForm =
+     `<div class="tooltip">${data.title}</div>
+      <input quickbeam="color" id="swatch-1-${data.type}" type="radio" name="color" value="${data.type}" ${block}>
+      <label for="swatch-1-${data.type}" style="border-color: ${data.type};">
+        <span style="background-color: ${data.type};"></span>
+        <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+      </label>`
+      item2.innerHTML=colorForm;
+      item2.addEventListener('click', e => {
+        e.stopPropagation();
+        let inp = item2.querySelector('input')
+        inp.setAttribute('checked', 'checked');
+        localStorage.setItem('color', e.target.value);
+      })
     }
-    item.appendChild(item2)
-    var item3 = document.createElement('div')
-    item3.className ='tooltip'
-    item3.innerHTML = data.title;
-    item2.appendChild(item3)
-    var item4 = document.createElement('input')
-    item4.setAttribute('quickbeam','color')
-    item4.id=`swatch-1-${data.type}`;
-    item4.setAttribute('type','radio')
-    item4.setAttribute('name','color')
-    item4.setAttribute('value', data.type)
-    item2.appendChild(item4)
-    if(data.isAvailable){
-      item4.removeAttribute('disabled')
-    } else{
-      item4.setAttribute('disabled', 'disabled')
-    }
-    var item5 = document.createElement('label')
-    item5.setAttribute('for', item4.id)
-    item5.setAttribute('style',`border-color: ${data.type}`)
-    item2.appendChild(item5)
-    var span = document.createElement('span')
-    span.setAttribute('style',`background-color: ${data.type}`)
-    item5.appendChild(span)
-    if(data.isAvailable){
-      var img = document.createElement('img')
-      img.className ='crossed-out'
-      img.setAttribute('src', document.getElementsByClassName('thumb-image')[i].href)
-      item5.appendChild(img)
-    } else return;
-    item4.addEventListener('click', e => {
-      e.target.setAttribute('checked', 'checked');
-    })
-    }
-}
+};
 
-const xhr2 = new XMLHttpRequest();
+const xhr2 = new XMLHttpRequest(); // добавление размеров
 xhr2.open('GET', 'https://neto-api.herokuapp.com/cart/sizes');
 xhr2.send();
 xhr2.addEventListener('load', onLoadSizes);
@@ -65,91 +64,187 @@ xhr2.addEventListener('load', onLoadSizes);
 function onLoadSizes(data) {
   var resultSizes = JSON.parse(xhr2.response)
   var item = document.querySelector('#sizeSwatch');
+
   for(var i=0; i < resultSizes.length; i++) {
-    addFormSizes(resultSizes[i])
+    addFormSizes(resultSizes[i]);
+    const sizeSnippets = item.querySelectorAll('input');
+    for(let el of sizeSnippets) {
+      el.checked=el.value === localStorage.getItem('size')
+    }
   }
 
   function addFormSizes(data) {
+    lockVariable(data);
     var item2 = document.createElement('div');
     item2.dataset.value = data.type;
     item2.className ='swatch-element plain';
     item2.classList.add(data.type);
-    if(data.isAvailable) {
-      item2.classList.add('available')
-    } else {
-      item2.classList.add('soldout')
-    }
+    item2.classList.add(access);
     item.appendChild(item2);
-    var input = document.createElement('input');
-    input.id=`swatch-0-${data.type}`;
-    input.setAttribute('type', 'radio');
-    input.setAttribute('name', 'size');
-    input.setAttribute('value', data.type);
-    if(data.isAvailable) {
-      input.removeAttribute('disabled')
-    } else {
-      input.setAttribute('disabled', 'disabled')
-    }
-    item2.appendChild(input);
-    var label = document.createElement('label');
-    label.setAttribute('for', input.id);
-    label.innerHTML = data.title;
-    item2.appendChild(label);
-
-    var img = document.createElement('img');
-    img.className ='crossed-out';
-    img.setAttribute('src', 'https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886');
-    label.appendChild(img)
+    let sizeform =
+    `<input id="swatch-0-${data.type}" type="radio" name="size" value="${data.type}" ${block}>
+      <label for="swatch-0-${data.type}">
+        ${data.title}
+        <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+      </label>`;
+    item2.innerHTML=sizeform;
     item2.addEventListener('click', e => {
-      input.setAttribute('checked', 'checked');
+      e.stopPropagation();
+      let inp = item2.querySelector('input')
+      inp.setAttribute('checked', 'checked');
+      localStorage.setItem('size', e.target.value);
     })
   }
 };
 
 const btnAddToCart = document.getElementById('AddToCart'); // кнопка
+let addToCartForm = document.getElementById('AddToCartForm');
 btnAddToCart.addEventListener('click', addProdToCart);
 
-function addProdToCart() {
-  event.preventDefault()
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://neto-api.herokuapp.com/cart');
-  xhr.addEventListener('load', () => {
-    let data = JSON.parse(xhr.response);
-    console.log(data[0]);
-   addSnipetCart(data[0])
-  });
-  const form = new FormData();
-  form.append('productId', '2721888517');
-  form.append('quantity', '1');
-  xhr.send(form);
+function addProdToCart(event) {
+  event.preventDefault();
+  const formData = new FormData(addToCartForm);
+  formData.append('productId', addToCartForm.dataset.productId);
+  fetch('https://neto-api.herokuapp.com/cart', {
+    body: formData,
+    credentials: 'same-origin',
+    method: 'POST'
+  })
+  .then((response) => {
+    if (response.status >= 200 && response.status < 400) {
+        return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then((formData) => buildQuick(formData))
+  .catch(console.error);
 };
 
-function addSnipetCart(data) {
-  let item = document.getElementById('quick-cart');
-  let text =
-`<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${data.id}" style="opacity: 1;">
-  <div class="quick-cart-product-wrap">
-    <img src="${data.pic}" title="${data.title}">
-    <span class="s1" style="background-color: #000; opacity: .5">$800.00</span>
-    <span class="s2"></span>
-  </div>
-  <span class="count hide fadeUp" id="quick-cart-product-count-${data.id}">${data.quantity}</span>
-  <span class="quick-cart-product-remove remove" data-id="${data.id}"></span>
-</div>`;
-item.innerHTML = text;
+  item.addEventListener('click', deleteProd) // удаление товара
 
-let quick = document.createElement('a');
-quick.id='quick-cart-pay';
-quick.setAttribute('quickbeam', 'cart-pay');
-quick.className='cart-ico';
-if(data.quantity>0) {
-  quick.classList.add('open')
-}
-let span =
+  function deleteProd(event) {
+    const formData = new FormData(addToCartForm);
+    formData.append('productId', addToCartForm.dataset.productId);
+    fetch('https://neto-api.herokuapp.com/cart/remove', {
+      body: formData,
+      credentials: 'same-origin',
+      method: 'POST'
+    })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 400) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((formData) => buildQuick(formData))
+    .catch(console.error);
+  };
+
+function lockVariable(data) { // блокировка снипетов
+  if(data.isAvailable) {
+    access = 'available';
+    block = '';
+  } else {
+    access = 'soldout';
+    block = 'disabled';
+  };
+};
+
+function buildQuick(data) { // содержимое корзины
+  let dt = data[0];
+  console.log(data);
+  if(dt === undefined) {
+    dt = {
+      id:0,
+      pic:'',
+      title:'',
+      price:0,
+      quantity:0
+    };
+    }
+  console.log(dt);
+  price = dt.quantity * dt.price;
+
+  let text =
+  `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${dt.id}" style="opacity: 1;">
+  <div class="quick-cart-product-wrap">
+  <img src="${dt.pic}" title="${dt.title}">
+  <span class="s1" style="background-color: #000; opacity: .5">$${price}</span>
+  <span class="s2"></span>
+  </div>
+  <span class="count hide fadeUp" id="quick-cart-product-count-${dt.id}">${dt.quantity}</span>
+  <span class="quick-cart-product-remove remove" data-id="${dt.id}"></span>
+  </div>`;
+  item.innerHTML = text;
+
+  let quick = document.createElement('a');
+  quick.id='quick-cart-pay';
+  quick.setAttribute('quickbeam', 'cart-pay');
+  quick.className='cart-ico';
+  if(dt.quantity>0) {
+    quick.classList.add('open')
+  } else {
+    quick.classList.remove('open')
+  }
+  let span =
   `<span>
-    <strong class="quick-cart-text">Оформить заказ<br></strong>
-    <span id="quick-cart-price">$${data.price}</span>
+  <strong class="quick-cart-text">Оформить заказ<br></strong>
+  <span id="quick-cart-price">$${price}</span>
   </span>`;
   quick.innerHTML = span;
   item.appendChild(quick);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
