6be542a1f0f38d7f9a20027f79c1f23c4fbce9dc
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2019-01-28 22:47:25 +0300

    28/01

--- HW/websocket/chat/js/chat.js
+++ HW/websocket/chat/js/chat.js
@@ -8,6 +8,7 @@ let messageInput = document.querySelector('.message-input');
 
 let message = document.getElementsByClassName('message')[1];
 let messagePersonal = document.getElementsByClassName('message')[2];
+
 let timestamp = document.querySelectorAll('.timestamp')
 
 const messages = document.getElementsByClassName('message');
@@ -18,13 +19,6 @@ let createData = function () {
 	let time = `${Hour}:${Minutes}`;
 	return time;
 }
-let timestamps = Array.from(timestamp)
-console.log(timestamps[0].innerHTML)
-
-timestamps.forEach(function(el,i) {
-	//el[i].innerHTML = createData();
-	console.log(el.i)
-})
 
 connection.addEventListener('open', () => {
 	chatStatus.innerHTML = chatStatus.dataset.online;
@@ -32,29 +26,23 @@ connection.addEventListener('open', () => {
 	messageStatus.innerHTML = 'Пользователь появился в сети';
 })
 
-
-
 connection.addEventListener('message', (event) => {
 	message.querySelector('.message-text').innerHTML = event.data
-	timestamp[0].innerHTML = time
+	timestamp[0].innerHTML = createData()
 	if(message.innerHTML === '...') {
 		document.getElementsByClassName('message')[0].innerHTML = 'Собеседник печатает сообщение';
 	} else {
 		document.getElementsByClassName('message')[0].innerHTML = ''
 	}
-	
 })
 
 messageSubmit.addEventListener('click', (event) => {
 	event.preventDefault()
 	messagePersonal.querySelector('.message-text').innerHTML = messageInput.value
-	timestamp[1].innerHTML = time
+	timestamp[1].innerHTML = createData()
 	connection.send(JSON.stringify(messageInput.value))
 	messageInput.value = ''
 })
-
-
-
 
 connection.onclose = () => {
 	chatStatus.innerHTML = chatStatus.dataset.offline;
@@ -76,6 +64,5 @@ function addtomessagesContent(o, p,m) {
 	document.querySelector('.messages-content').appendChild(m)
 	return document.querySelector('.messages-content');
 }
-
 
 console.log(addtomessagesContent(messageStatus, message, messagePersonal))
