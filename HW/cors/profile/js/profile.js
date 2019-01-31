62a5571ca4f8c92163e9ad72c0d409273d87a1c7
vertjs <lena.vertieva@gmail.com>
2019-01-31 16:09:14 +0300

    31/01

--- HW/cors/profile/js/profile.js
+++ HW/cors/profile/js/profile.js
@@ -1,24 +1,32 @@
 'use strict';
 
-function loadData(url) {
-	
-	var elem = document.createElement("script");
-	elem.setAttribute("type", "text/javascript");
-	elem.src = url;
-	document.body.appendChild(elem);
-	}
 
-
-loadData('https://neto-api.herokuapp.com/profile/me')
-callback(loadData)
+function addScript(src) {
+  var elem = document.createElement("script");
+  elem.setAttribute("type", "text/javascript");
+  elem.src = src;
+  document.head.appendChild(elem);
+}
+addScript('https://neto-api.herokuapp.com/profile/me');
 
 function callback(data) {
+	document.querySelector('[data-name]').innerHTML = data.name;
+	document.querySelector('[data-description]').innerHTML = data.description;
+	document.querySelector('[data-pic]').src = data.pic;
+	document.querySelector('[data-position]').innerHTML = data.position;
+	document.querySelector('.content').style.display = 'initial';
 
-console.log(data);
+	return console.log(`https://neto-api.herokuapp.com/profile/${data.id}/technologies`);
 }
 
-fetch('https://neto-api.herokuapp.com/profile/me').then(function(response) {
-	return console.log(responseText);
-}).then(function(response) {
-	var objectURL = URL.createObjectURL(response)
+
+function foo(data) {
+	console.log(data);
+/*	document.querySelector('[data-following]').innerHTML = data[0];
+
+	document.querySelector('[data-technologies]').innerHTML = data.technologies;
+	var span = document.createElement("span");
+	span.className ='devicons'
+	span.classList.add('devicons-django')*/
 }
+foo(callback())
