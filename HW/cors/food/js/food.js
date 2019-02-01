0df3fd8acb43564a8267ba41b5a88beb4c59d5ca
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2019-02-02 00:55:21 +0300

    02/02

--- HW/cors/food/js/food.js
+++ HW/cors/food/js/food.js
@@ -8,16 +8,34 @@ function addScript(callbackName, url) {
     document.body.appendChild(elem);
     window.callbackName = done;
     console.log(callbackName, url);
-})
-}
+  })
+};
 
 addScript('recipe', 'https://neto-api.herokuapp.com/food/42');
 
 function recipe(data) {
-  const title = document.querySelector('[data-title]');
-  title.innerHTML = data.title
-  const pic = document.createElement('image');
-  sp.classList.add('devicons', `devicons-${data[d]}`);
-  technologies.appendChild(sp);
-}
+  const pic = document.querySelector('[data-pic]');
+  pic.style.backgroundImage = `url(${data.pic})`;
+  document.querySelector('[data-title]').textContent = data.title;
+  document.querySelector('[data-ingredients]').textContent = data.ingredients;
+};
+
+addScript('addRating', 'https://neto-api.herokuapp.com/food/42/rating');
+
+function addRating(data) {
+  document.querySelector('[data-rating]').textContent = data.rating;
+  document.querySelector('[data-votes]').textContent = `(${data.votes} Оценок)`;
+};
+
+addScript('addConsumers', 'https://neto-api.herokuapp.com/food/42/consumers');
+
+function addConsumers(data) {
+  for(let key in data.consumers) {
+    let i = document.createElement("img");
+    i.src = data.consumers[key].pic;
+    i.title = data.consumers[key].name;
+    console.log(i);
+    document.querySelector('[data-consumers]').appendChild(i)
+  }
+
 }
