cad45696dde08ec939466ad36bb7a87dea7e94b8
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2019-03-28 22:58:06 +0300

    added editinq of comments

--- HW/dom-from-zero/comments/js/comments.js
+++ HW/dom-from-zero/comments/js/comments.js
@@ -3,50 +3,34 @@
 function showComments(list) {
   const commentsContainer = document.querySelector('.comments');
   const comments = list.map(createComment);
-  const fragment = comments.reduce((prev, cur) => {
-    prev.appendChild(cur);
-    return prev;
-  }, document.createDocumentFragment());
-
-  commentsContainer.appendChild(fragment);
-  commentsContainer.style.whiteSpace = 'pre-line';
-}
-
-function el(tag, attr, child){
-  const elem = document.createElement(tag);
-  if(typeof attr === 'object'){
-    Object.keys(attr).forEach(el => elem.setAttribute(el, attr[el]));
-  }
-  if(typeof child === 'string' || typeof child === 'number'){
-    elem.textContent = child;
-  } else if (child instanceof Array){
-    child.forEach(el => elem.appendChild(el))
-  }
-  return elem;
+  commentsContainer.innerHTML += comments;
 }
 
 function createComment(comment) {
-  return el('div', {class: "comment-wrap"}, [
-    el('div', {class: "photo", title: comment.author.name}, [
-       el('div', {class: "avatar", style: `background-image: url(${comment.author.pic})` })
-    ]),
-     el('div', {class: "comment-block"}, [
-      el('p', {class: "comment-text"}, `${comment.text}`),
-      el('div', {class: "bottom-comment"}, [
-        el('div', {class: "comment-date"}, `${new Date(comment.date).toLocaleString('ru-Ru')}`),
-        el('ul', {class: "comment-actions"}, [
-          el('li', {class: "complain"}, 'Пожаловаться'),
-          el('li', {class: "reply"}, 'Ответить')
-        ]),
-      ]),
-    ])
-  ]);
+  return `<div class="comment-wrap">
+    <div class="photo" title="${comment.author.name}">
+      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
+    </div>
+    <div class="comment-block">
+      <p class="comment-text">
+        ${comment.text.split('\n').join('<br>')}
+      </p>
+      <div class="bottom-comment">
+        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
+        <ul class="comment-actions">
+          <li class="complain">Пожаловаться</li>
+          <li class="reply">Ответить</li>
+        </ul>
+      </div>
+    </div>
+  </div>`
 }
 
-async function loadData () {
-    const load = await fetch('https://neto-api.herokuapp.com/comments');
-    const parse = await load.json();
-    showComments(parse);
+async function f() {
+  let response = await fetch('https://neto-api.herokuapp.com/comments');
+  let res = await response.json();
+  console.log(res);
+  await showComments(res);
 }
-
-loadData();
+f();
+//.then(showComments);
